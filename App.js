f6de6db815d62b182db9f1531f734d21a4825c4b
Rene Correa <ratanky@hotmail.com>
2021-05-26 01:33:34 -0300

    First Commit

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+
+import LoadingScreen from './screens/LoadingScreen';
+import LoginScreen from './screens/LoginScreen';
+import DashboardScreen from './screens/DashboardScreen';
+
+import * as firebase from 'firebase';
+import{firebaseConfig} from './config';
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    AppNavigator
   );
 }
+
+const AppSwitchNavigator = createSwitchNavigator({
+  LoadingScreen:LoadingScreen,
+  LoginScreen:LoginScreen,
+  DashboardScreen:DashboardScreen
+})
+
+const AppNavigator = createAppContainer (AppSwitchNavigator);
 
 const styles = StyleSheet.create({
   container: {
---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAHNiz3ydCkCAIwGATGOaL4WjwyWEcAu6o",
+    authDomain: "ac2-mobile-36d80.firebaseapp.com",
+    projectId: "ac2-mobile-36d80",
+    storageBucket: "ac2-mobile-36d80.appspot.com",
+    messagingSenderId: "476821295329",
+    appId: "1:476821295329:web:aee241d06d9efbb72267a0",
+    measurementId: "G-X3RQB7KW4J"
+};
